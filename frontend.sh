c38a5badccf5e9b1371347285d66f96a6634cb5b
bharath-gaveni <bharathgaveni@gmail.com>
2023-11-04 12:50:50 +0530

    changes in files because copying file name to location.

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,22 @@
 
 echo -e "\e[31m installing nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/expense.log
 
 
-
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[31m enabling nginx \e[0m"
+systemctl enable nginx &>>/tmp/expense.log
+echo -e "\e[31m starting nginx \e[0m"
+systemctl start nginx &>>/tmp/expense.log
 echo -e "\e[31m coping expense.conf file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 echo -e "\e[31m removing content in nginx file \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 echo -e "\e[31m dowloading frontend application content \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
-
-cd /usr/share/nginx/html
+echo -e "\e[31m changing location \e[0m"
+cd /usr/share/nginx/html &>>/tmp/expense.log
 echo -e "\e[31m unzipping the content in temporary file \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 echo -e "\e[31m restarting the nginx  \e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>/tmp/expense.log
