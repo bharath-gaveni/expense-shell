32f77597b83f26dc8b4c3298cb7bebd200ca32c8
bharath-gaveni <bharathgaveni@gmail.com>
2023-11-04 14:01:33 +0530

    changes in files because copying file name to location.

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,23 @@
+log_file=/tmp/expense.log
+clour="\e[31m"
+echo -e "${clour} installing nginx \e[0m"
+dnf install nginx -y &>>$log_file
 
-echo -e "\e[31m installing nginx \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
 
+echo -e "${clour} enabling nginx \e[0m"
+systemctl enable nginx &>>$log_file
+echo -e "${clour} starting nginx \e[0m"
+systemctl start nginx &>>$log_file
+echo -e "${clour} coping expense.conf file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo -e "${clour} removing content in nginx file \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo -e "${clour} dowloading frontend application content \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
-echo -e "\e[31m enabling nginx \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-echo -e "\e[31m starting nginx \e[0m"
-systemctl start nginx &>>/tmp/expense.log
-echo -e "\e[31m coping expense.conf file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
-echo -e "\e[31m removing content in nginx file \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
-echo -e "\e[31m dowloading frontend application content \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
-
-echo -e "\e[31m changing location \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-echo -e "\e[31m unzipping the content in temporary file \e[0m"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
-echo -e "\e[31m restarting the nginx  \e[0m"
-systemctl restart nginx &>>/tmp/expense.log
+echo -e "${clour} changing location \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+echo -e "${clour} unzipping the content in temporary file \e[0m"
+unzip /tmp/frontend.zip &>>$log_file
+echo -e "${clour} restarting the nginx  \e[0m"
+systemctl restart nginx &>>$log_file
