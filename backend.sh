891e9ae7f10557a23e1fbc38618c5dde894dccd6
bharath-gaveni <bharathgaveni@gmail.com>
2023-11-06 01:29:08 +0530

    adding function

--- backend.sh
+++ backend.sh
@@ -16,9 +16,11 @@ status_check() {
 echo -e "${clour} disable nodejs \e[0m"
 dnf module disable nodejs -y &>>log_file
 status_check()
+
 echo -e "${clour} enabling nodejs 18 version \e[0m"
 dnf module enable nodejs:18 -y &>>log_file
 status_check()
+
 echo -e "${clour} installing nodejs18 \e[0m"
 dnf install nodejs -y &>>log_file
 status_check()
