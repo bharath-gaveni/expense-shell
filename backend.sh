fd660c96377a4da3bf18b72f4491e946235f994d
bharath-gaveni <bharathgaveni@gmail.com>
2023-11-05 22:23:31 +0530

    adding the exit status message to backend.sh...

--- backend.sh
+++ backend.sh
@@ -3,94 +3,118 @@ log_file="/tmp/expense.log"
 clour="\e[36m"
 echo -e "${clour} disable nodejs \e[0m"
 dnf module disable nodejs -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} enabling nodejs 18 version \e[0m"
 dnf module enable nodejs:18 -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} installing nodejs18 \e[0m"
 dnf install nodejs -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} installing mysql client \e[0m"
 dnf install mysql -y &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
-
+fi
 echo -e "${clour} adding user \e[0m"
 useradd expense &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} copying backend service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} making a directory for application content to store \e[0m"
 mkdir /app &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} removing old application content in app directory and it is free \e[0m"
 rm -rf /app/* &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
-
+fi
 echo -e "${clour}downloading backend application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} changing the directory \e[0m"
 cd /app &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} unzipping the application content file in app directory  \e[0m"
 unzip /tmp/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} changing the directory\e[0m"
 cd /app &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} dowloading dependencies for nodejs software \e[0m"
 npm install &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} reloading and starting the nodejs  \e[0m"
 systemctl daemon-reload &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 systemctl enable backend &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 systemctl start backend &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 echo -e "${clour} loading schema to mysql client \e[0m"
 mysql -h mysql-dev.bharathgaveni.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
