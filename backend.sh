b2b77b58e6a292f49399becc7b49cacbcce39942
bharath-gaveni <bharathgaveni@gmail.com>
2023-11-05 22:58:13 +0530

    adding the exit status message to backend.sh...

--- backend.sh
+++ backend.sh
@@ -29,6 +29,8 @@ if [ $? -eq 0 ]; then
 else
   echo -e "\e[31m FAILURE \e[0m"
 fi
+id expense &>>log_file
+if [ $? -ne 0]; then
 echo -e "${clour} adding user \e[0m"
 useradd expense &>>log_file
 if [ $? -eq 0 ]; then
